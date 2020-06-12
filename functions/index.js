1d61017fba4f6a89aaa564f5adae7278c775792b
goldenstar07 <ssinezdev07@gmail.com>
2020-06-11 17:08:10 -0700

    add wyre

--- functions/index.js
+++ functions/index.js
@@ -5,9 +5,9 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 const CryptoJS = require('crypto-js');
 const app = express();
-const URL = 'https://sandboxapi.rapyd.net';
-const key = '60043F6D3C676B824DF1';
-const secret = '0f658c5917dbec956fb06a7cd21edb6f6b1518c7b8fd13bceb261d5d970922f57252fa267a036c33';
+const URL = 'https://api.testwyre.com';
+const key = 'AK-DP87RF9M-HXL9HRL6-HJBB36TP-TXJFAPMT';
+const secret = 'SK-363QJB7U-GP6LQRBT-VQFPP7BG-87PD9YHQ';
 
 app.use(cors({ origin: true }));
 app.use(express.static('public'));
@@ -16,36 +16,48 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
-app.post('/create', (req, res) => {
-  let salt = Math.abs(CryptoJS.lib.WordArray.random(12).words[0]);
+app.get('/create', (req, res) => {
+  //salt - Math.abs(CryptoJS.lib.WordArray.random(12).words[0]);
   let timestamp = (Math.floor(new Date().getTime() / 1000) - 10).toString();
-  let uri = '/v1/user';
-  let fullUri = `${URL}/v1/user`;
-  let method = "post";
+  let uri = '/v3/accounts';
+  let fullUri = `${URL}${uri}?timestamp=${timestamp}`;
   let body = '';
-  if(req.body.phone_number.toString() != '{}' && req.body.phone_number.toString() != ''){
-    body = {
-      first_name: req.body.first_name.toString(),
-      last_name: req.body.last_name.toString(),
-      phone_number: req.body.phone_number.toString(),
-      email: req.body.email.toString(),
-      type: req.body.type.toString(),
-      business_details: {},
-      metadata: {
-        merchant_defined: true
+  body = {
+    type: 'INDIVIDUAL',
+    country: 'US',
+    subaccount: true,
+    profileFields:[
+      {
+        "fieldId": "individualLegalName",
+        "value": req.body.name.toString()
+      },
+      {
+        "fieldId": "individualEmail",
+        "value": req.body.email.toString()
+      },
+      {
+        "fieldId": "individualResidenceAddress",
+        "value": {
+          "street1": req.body.street1.toString(),
+          "street2": req.body.street2.toString(),
+          "city": req.body.city.toString(),
+          "state": req.body.state.toString(),
+          "postalCode": req.body.zip.toString(),
+          "country": req.body.country.toString()
+        }
       }
-    } 
-  }
+    ]
+  } 
   let details = JSON.stringify(body);
-  let to_sign = method + uri + salt + timestamp + key + secret + details;
+  let to_sign = fullUri + details;
   let signature = CryptoJS.enc.Hex.stringify(CryptoJS.HmacSHA256(to_sign, secret));
+  console.log(signature);
   signature = CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(signature));
+  console.log(signature);
   let headers = {};
-  headers['access_key'] = key;
   headers['Content-Type'] = 'application/json';
-  headers['salt'] = salt;
-  headers['signature'] = signature;
-  headers['timestamp'] = timestamp;
+  headers['X-Api-Key'] = key;
+  headers['X-Api-Signature'] = signature;
 
   const config = {
       method: "POST",
@@ -57,9 +69,9 @@ app.post('/create', (req, res) => {
       if (response.data){
         res.send(response.data);
       }
-    }).catch(function (error) {
+    }).catch((error) =>{
       if (error.response) {
-        res.send(error.response, error.response.data);
+        res.send(error.response.data);
       } else if (error.request) {
         res.send(error.request);
       } else {
@@ -69,4 +81,4 @@ app.post('/create', (req, res) => {
     });
 });
   
-exports.rapyd = functions.https.onRequest(app);
+exports.wyre = functions.https.onRequest(app);
